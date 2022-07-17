bf85b74536014febb7a02e1474a77b947fc98cdd
Chris <christopherhpena02@gmail.com>
2022-07-17 15:23:40 -0400

    added functions to buttons and clear button should set value to 0, operator buttons still need work.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 /*var btn = document.getElementById('btn');
 var num = document.getElementById('num');
-
-
 var btnVal = btn.textContent;
 var numVal = num.textContent;
-
 btn.addEventListener('click',
     function handleClick() {
         var tempNum = parseFloat(numVal) + parseFloat(btnVal) ;
-        num.textContent = btn.textContent;
+        num.textContent = "3333";
     }
 )*/
 
@@ -24,16 +21,34 @@ btn.addEventListener('click',
         numVal.textContent = btn.textContent;
         }
     )*/
+try{
+        var btns = document.getElementsByClassName("button");
+        for(var i = 0; i < btns.length; i++) {
+                btns[i].addEventListener("click", function(event)
+                    {
 
-btns = document.getElementsByClassName("button");
-for(var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function()
-                {
-                let num = document.getElementById('num').textContent;
-                let numVal = parseFloat(num);
-                let btnVal = parseFloat(btns[i].textContent);
-                num = numVal + btnVal;
+                            let num = document.getElementById('num');
+                            let temp1 = event.target.innerHTML;
+                            let numVal = parseFloat(num.textContent);
+                            let btnVal = parseFloat(temp1);
 
-                }
-        )
-}
+                            if(num.textContent == "0"){
+                                    num.textContent = temp1;
+                            }else{num.textContent = numVal + temp1;}
+
+                    }
+                )
+        }}catch(e){}
+
+try{
+        clear = document.getElementById("clearBtn");
+        clear.addEventListener("click", function()
+        {
+                num.textContent = "0";
+        })
+}catch(e){}
+
+
+
+
+

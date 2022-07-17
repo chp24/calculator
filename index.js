01989cfdbec095e1939e6c54af2a610ee56d8093
Chris <christopherhpena02@gmail.com>
2022-07-17 19:01:54 -0400

    added functioning addition and equals

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-try{
+ let num = document.getElementById('num');
         var btns = document.getElementsByClassName("button");
         for(var i = 0; i < btns.length; i++)
         {
                 btns[i].addEventListener("click", function(event)
                     {
-                            let num = document.getElementById('num');
+
                             let temp1 = event.target.innerHTML;
                             let numVal = num.textContent;
                             if(numVal == "0")
@@ -18,14 +18,26 @@ try{
                     }
                 )
         }
-}catch(e){ console.error("oops",e)}
-try{
-        clear = document.getElementById("clearBtn");
+    add = document.getElementById('addBtn');
+    let currentNum = 0;
+        add.addEventListener("click", function()
+            {
+                currentNum = currentNum + parseFloat(num.textContent);
+                num.textContent = 0;
+            }
+        )
+    eq = document.getElementById("eqBtn");
+        eq.addEventListener('click', function ()
+            {
+                num.textContent = currentNum + parseFloat(num.textContent);
+            }
+        )
+    clear = document.getElementById("clearBtn");
         clear.addEventListener("click", function()
         {
-                num.textContent = "0";
+            num.textContent = "0";
+            currentNum = 0;
         })
-}catch(e){console.error("oops",e)}
 
 
 
